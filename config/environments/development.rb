9b63c43c68cc6a758b6d0deb6ba821975da0a740
yoshinori.kodama <acedia-belphegor.7@gmail.com>
2026-07-22 10:26:48 +0900

    add: host.docker.internalをHostAuthorizationの許可リストに追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Docker コンテナ(fhir-mcp-server 等)からは host.docker.internal 経由で
+  # アクセスされるため、HostAuthorization の許可リストに加える。
+  config.hosts << "host.docker.internal"
+
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
